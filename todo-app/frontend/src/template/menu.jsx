2d49f219351a665a6189b000b90edf25eb32460a
rdfenix <rhoodsk8erboy@gmail.com>
2018-02-05 23:48:31 -0200

    Menu de Rotas Criadas

--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -7,7 +7,7 @@ export default (props) => (
                 <a href="#" className="navbar-brand"><i className="fa fa-calendar-check-o"></i> TodoApp</a>
             </div>
             <div id="navbar" className="navbar-collapse collapse">
-                <ul className="nav navbar">
+                <ul className="nav navbar-nav">
                     <li><a href="#/todos">Tarefas</a></li>
                     <li><a href="#/about">Sobre</a></li>
                 </ul>
