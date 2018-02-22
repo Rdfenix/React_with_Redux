df2eb0bc577fff6d82813d6b4042a2d104d7dae5
rdfenix <rhoodsk8erboy@gmail.com>
2018-02-22 00:31:21 -0300

    Desenvolvendo backend

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -11,3 +11,5 @@ server.use(bodyParser.json())
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`);
 });
+
+module.exports = server;
--- my-money-app/backend/src/loader.js
+++ my-money-app/backend/src/loader.js
@@ -1,3 +1,4 @@
-require('./config/server');
+const server = require('./config/server');
 require('./config/database');
+require('./config/routes')(server);
 
