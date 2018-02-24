9bc0ffdd11ab9f9deb22b2f10db53e0423e4b836
rdfenix <rhoodsk8erboy@gmail.com>
2018-02-24 12:25:04 -0300

    Backend terminado

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,10 +3,14 @@ const port = 3003;
 const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
+const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 //middleware
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json())
+server.use(allowCors);
+server.use(queryParser());
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`);
