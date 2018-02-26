e0ebd59c8a9022a417695365582b41f73a705e09
rdfenix <rhoodsk8erboy@gmail.com>
2018-02-26 08:20:33 -0300

    Criando a arvore de menus no front-end

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import '../common/template/dependencies'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 export default (props) => (
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer />
     </div>
 )
