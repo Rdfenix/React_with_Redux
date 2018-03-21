3ec458b92f33c5195d7575806e9fb6a2b9a76eee
rdfenix <rhoodsk8erboy@gmail.com>
2018-03-21 08:41:45 -0300

    Criando o summary - backend

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -37,17 +37,17 @@ BillingCycle.route('summary', (req, res, next) => {
         }
     }, {
         $project: {
-            _id:0,
+            _id: 0,
             credit: 1,
             debt: 1
         }
     }, (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]});
-        } else {
-            res.json(result[0] || {credit: 0, debt: 0});
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json(result[0] || {credit: 0, debt: 0})
         }
-    });
+    })
 });
 
 module.exports = BillingCycle;
--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,19 +1,36 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 
+import {getSummary} from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+
+    componentWillMount(){
+        this.props.getSummary()
+    }
+
     render(){
+        const { credit, debt } = this.props.summary
         return(
             <div>
                 <ContentHeader title="Dashboard" subtitle="versão 1.0" />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols="12 4" color="green" icon="bank" value={`R$ ${credit}`} text="Total de créditos" />
+                        <ValueBox cols="12 4" color="red" icon="credit-card" value={`R$ ${debt}`} text="Total de débitos" />
+                        <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${credit - debt}`} text="Valor consolidado" />
+                    </Row>
                 </Content>
             </div>
         )
     }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => ({summary: state.dashboard.summary})
+const mapDispatchToProps = (dispatch) => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
